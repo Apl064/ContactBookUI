a500ef29d04e7906c215e0fb8b12fd9db962faa9
apl064 <apl064@gmail.com>
2022-05-25 23:58:21 +0300

    Initial Commit

---
+++ ContactBookUI/ContactBookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactBookUIApp.swift
+//  ContactBookUI
+//
+//  Created by Pavel Metelin on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactBookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
